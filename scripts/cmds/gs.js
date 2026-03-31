cb6a7f2f87f0d711ff82df4e3e091615b90a8b8d
𝐀 𝐑 𝐈 𝐉 𝐈 𝐓⚡ <naruto1629u@gmail.com>
2026-03-31 16:08:59 +0530

    Update gs.js

--- scripts/cmds/gs.js
+++ scripts/cmds/gs.js
@@ -16,7 +16,7 @@ module.exports = {
     longDescription: {
       en: "📌 Browse, search, upload, and manage your commands in the GoatStore marketplace with easy sharing cmds."
     },
-    category: "𝗠𝗮𝗿𝗸𝗲𝘁",
+    category: "admin",
     cooldowns: 0,
   },
 
