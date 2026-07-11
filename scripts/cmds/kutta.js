291dd7ccdb5a0e759adca7a758cb663ec36c03c8
𝐀 𝐑 𝐈 𝐉 𝐈 𝐓⚡ <naruto1629u@gmail.com>
2026-07-11 10:26:52 +0530

    Update kutta.js

--- scripts/cmds/kutta.js
+++ scripts/cmds/kutta.js
@@ -28,7 +28,7 @@ module.exports.onStart = async function ({ api, event, message }) {
     const senderID = event.senderID;
 
     // 🚫 Owner protection
-    if (targetID === "61573866391878" && senderID !== "61573866391878") {
+    if (targetID === "61591262776146" && senderID !== "61591262776146") {
       return message.reply("🚫 You deserve this, not my owner! 😙");
     }
 
