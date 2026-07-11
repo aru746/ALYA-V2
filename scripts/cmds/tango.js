4e5d4247f1f968e01119672c951eef3446f0cef3
𝐀 𝐑 𝐈 𝐉 𝐈 𝐓⚡ <naruto1629u@gmail.com>
2026-07-11 10:27:16 +0530

    Update tango.js

--- scripts/cmds/tango.js
+++ scripts/cmds/tango.js
@@ -24,7 +24,7 @@ module.exports.onStart = async function ({ api, event, message }) {
     const senderID = event.senderID;
 
     // 🚫 Owner protection: only block if someone else tries to Tango you
-    if (targetID === "61573866391878" && senderID !== "61573866391878") {
+    if (targetID === "61591262776146" && senderID !== "61591262776146") {
       return message.reply("🚫 You deserve this, not my owner! 😙");
     }
 
