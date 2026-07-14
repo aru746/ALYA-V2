1f420237716a79cea109bbff0aa0b46551c767b8
𝐀 𝐑 𝐈 𝐉 𝐈 𝐓⚡ <naruto1629u@gmail.com>
2026-07-14 14:23:40 +0530

    Update 0Kuze.js

--- scripts/cmds/0Kuze.js
+++ scripts/cmds/0Kuze.js
@@ -15,7 +15,7 @@ onChat: async function({ event, message, getLang }) {
 if (event.body && event.body.toLowerCase() === "kuze") {
 return message.reply({
 body: `
-  ➢ 𝐎𝐰𝐧𝐞𝐫 : 𝙰𝚁𝙸 𝙹𝙸𝚃 ♫︎
+  ➢ 𝐎𝐰𝐧𝐞𝐫 : 𝙺𝚞𝚉𝚎 ♫︎
 
  𝐣𝐮𝐬𝐭 𝐬𝐚𝐲 𝐛𝐨𝐭/𝐛𝐛𝐲 𝐟𝐨𝐫 𝐭𝐚𝐥𝐤 𝐭𝐨 𝙰𝙻𝚈𝙰 𝙱𝙾𝚃 ♫︎
  𝐞𝐧𝐣𝐨𝐲 𝐚𝐧𝐝 𝐡𝐚𝐯𝐞 𝐚 𝐟𝐮𝐧 𝐰𝐢𝐭𝐡 𝐦𝐲 𝐛𝐨𝐭
